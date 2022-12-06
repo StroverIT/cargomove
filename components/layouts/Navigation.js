e676de4c27c3199d9d0d9ed2f07a402fb2797fa5
StR0vErBG <xcreaw1@abv.bg>
2022-12-06 14:04:10 +0200

    adding navigation

--- components/layouts/Navigation.js
+++ components/layouts/Navigation.js
@@ -1,5 +1,5 @@
 import React, { useContext, useEffect, useState } from "react";
-import { FaFacebook, FaInstagram } from "react-icons/fa";
+import { FaFacebook, FaInstagram, FaLocationArrow } from "react-icons/fa";
 import { Twirl as Hamburger } from "hamburger-react";
 import { BsTelephoneForward } from "react-icons/bs";
 import { globalContext } from "../globalContext";
@@ -8,9 +8,35 @@ import Link from "next/link";
 import { useRouter } from "next/router";
 import Image from "next/legacy/image";
 import Logo from "../Logo";
-const hamburgerVariants = {
-  animate: { transition: { staggerChildren: 0.1, delay: 1 } },
-};
+import { BiRightArrow } from "react-icons/bi";
+import { TbBus } from "react-icons/tb";
+import { HiX } from "react-icons/hi";
+const lists = [
+  {
+    text: "Преместване на дома",
+    link: "home-moving",
+  },
+  {
+    text: "Преместване на офиса",
+    link: "office-moving",
+  },
+  {
+    text: "Международно преместване",
+    link: "international-relocation",
+  },
+  {
+    text: "Изхвърляне и извозване на стари мебели",
+    link: "disposal-of-old-furniture",
+  },
+  {
+    text: "Изхвърляне и на строителни отпадъци",
+    link: "disposal-of-construction-waste",
+  },
+  {
+    text: "Почистване на мазета и тавани",
+    link: "cleaning-basements-and-attics",
+  },
+];
 // transition: {
 //   type: "spring",
 //   duration: 1,
@@ -32,6 +58,7 @@ export default function Navigation() {
   const pathname = router.pathname;
   const [menuOpen, setMenuOpen] = useState(false);
   const [isOnMain, setIsOnMain] = useState(false);
+  const [servicePcState, setServicePcState] = useState(false);
 
   const handleNav = (hash, e) => {
     e.preventDefault();
@@ -66,7 +93,16 @@ export default function Navigation() {
     } else {
       setIsOnMain(false);
     }
+    setServicePcState(false);
   }, [router]);
+  useEffect(() => {
+    const body = document.querySelector("body");
+    if (menuOpen) {
+      body.style.overflow = "hidden";
+    } else if (!menuOpen) {
+      body.removeAttribute("style");
+    }
+  }, [menuOpen]);
   return (
     <nav
       className={`${
@@ -103,8 +139,8 @@ export default function Navigation() {
           </div>
         </div>
       </section> */}
-      <section className="container justify-between hidden lg:flex items-center">
-        <div className="h-16 w-56">
+      <section className="container items-center justify-between hidden lg:flex">
+        <div className="w-56 h-16">
           <Logo />
         </div>
         <section className="flex items-center justify-center gap-x-5">
@@ -116,11 +152,82 @@ export default function Navigation() {
               <li>
                 <Link href="/">Начало</Link>
               </li>
-              {pathname == "/" && (
-                <li>
-                  <a href="#services">Услуги</a>
-                </li>
-              )}
+
+              <li
+                className=" group"
+                onMouseEnter={(e) => setServicePcState(true)}
+                onMouseLeave={(e) => setServicePcState(false)}
+              >
+                <div className="cursor-pointer flex-center">
+                  <span className="">Услуги</span>
+                  <span
+                    className={`pl-1 group-hover:rotate-90 transition-transform text-lg`}
+                  >
+                    <TbBus />
+                  </span>
+                </div>
+                <AnimatePresence mode="wait">
+                  {servicePcState && (
+                    <motion.div
+                      initial="initialState"
+                      animate="animateState"
+                      exit="exitState"
+                      transition={{
+                        duration: 0.5,
+                        type: "spring",
+                      }}
+                      variants={{
+                        initialState: {
+                          y: "-100vh",
+                        },
+                        animateState: {
+                          y: 0,
+                        },
+                        exitState: {
+                          y: "-100vh",
+                          zIndex: -10,
+                        },
+                      }}
+                      className="relative flex flex-col w-full h-full "
+                    >
+                      <div className="absolute top-0 w-[26.5rem] h-10  -left-16"></div>
+                      <ul className="absolute flex-col  py-4 pl-10 list-disc w-[26.5rem] rounded-md bg-orange-150 top-10 -left-16 flex  shadow-2xl">
+                        {lists.map((list, i) => {
+                          return (
+                            <Link href={list.link} key={i}>
+                              <motion.li
+                                variants={{
+                                  hidden: (i) => ({
+                                    opacity: 0,
+                                    y: -50 * i,
+                                    zIndex: -10,
+                                  }),
+                                  visible: (i) => ({
+                                    opacity: 1,
+                                    y: 0,
+                                    zIndex: 0,
+
+                                    transition: {
+                                      delay: i * 0.05,
+                                    },
+                                  }),
+                                }}
+                                initial="hidden"
+                                animate="visible"
+                                custom={i}
+                                className="px-2 py-2 transition-colors cursor-pointer hover:bg-white hover:text-orange-150"
+                              >
+                                {list.text}
+                              </motion.li>
+                            </Link>
+                          );
+                        })}
+                      </ul>
+                    </motion.div>
+                  )}
+                </AnimatePresence>
+              </li>
+
               {pathname == "/" && (
                 <li>
                   <a href="#pricing">Цени</a>
@@ -138,15 +245,16 @@ export default function Navigation() {
                   </Link>
                 </li>
               )}
-              <a href="tel:+359895063670">
-                <li className="px-5 py-2 rounded-sm bg-orange">позвъни сега</li>
-              </a>
+
+              <li className="px-5 py-2 rounded-sm bg-orange">
+                <a href="tel:+359895063670">позвъни сега </a>
+              </li>
             </ul>
           </section>
         </section>
       </section>
       <section className="container justify-between flex-center lg:hidden ">
-        <div className="relative w-96 h-10">
+        <div className="relative h-10 w-96">
           <Logo />
         </div>
         <section className="flex items-center justify-center gap-x-5 ">
@@ -190,11 +298,21 @@ export default function Navigation() {
                     <li onClick={handleNav.bind({}, "#index")}>
                       <Link href="/">Начало</Link>
                     </li>
-                    {pathname == "/" && (
-                      <li onClick={handleNav.bind({}, "#services")}>
-                        <a href="#">Услуги</a>
-                      </li>
-                    )}
+
+                    <li
+                      className=" group"
+                      onClick={(e) => setServicePcState(true)}
+                    >
+                      <div className="cursor-pointer flex-center">
+                        <span className="">Услуги</span>
+                        <span
+                          className={`pl-1 group-hover:rotate-90 transition-transform text-lg`}
+                        >
+                          <TbBus />
+                        </span>
+                      </div>
+                    </li>
+
                     {pathname == "/" && (
                       <li onClick={handleNav.bind({}, "#pricing")}>
                         <a href="#">Цени</a>
@@ -215,6 +333,73 @@ export default function Navigation() {
                   </ul>
                 </section>
               </motion.section>
+            )}
+          </AnimatePresence>
+          <AnimatePresence mode="wait">
+            {servicePcState && (
+              <motion.div
+                initial="initialState"
+                animate="animateState"
+                exit="exitState"
+                transition={{
+                  duration: 0.5,
+                  type: "spring",
+                }}
+                variants={{
+                  initialState: {
+                    y: "-100vh",
+                  },
+                  animateState: {
+                    y: 0,
+                  },
+                  exitState: {
+                    y: "-100vh",
+                  },
+                }}
+                className="fixed top-0 left-0 z-[999] flex flex-col w-full h-screen"
+              >
+                <section className="w-full h-screen flex-center bg-orange-150">
+                  <section className="container relative">
+                    <section
+                      className="absolute right-0 text-xl bg-white rounded-md top-5 text-orange-150"
+                      onClick={() => setServicePcState(false)}
+                    >
+                      <HiX />
+                    </section>
+                    <ul className="flex-col p-10 text-sm ">
+                      {lists.map((list, i) => {
+                        return (
+                          <Link href={list.link} key={i}>
+                            <motion.li
+                              variants={{
+                                hidden: (i) => ({
+                                  opacity: 0,
+                                  y: -50 * i,
+                                  zIndex: -10,
+                                }),
+                                visible: (i) => ({
+                                  opacity: 1,
+                                  y: 0,
+                                  zIndex: 0,
+                                  transition: {
+                                    delay: i * 0.05,
+                                  },
+                                }),
+                              }}
+                              initial="hidden"
+                              animate="visible"
+                              custom={i}
+                              className="py-2 pl-2 transition-colors cursor-pointer hover:bg-white hover:text-orange-150"
+                            >
+                              {list.text}
+                            </motion.li>
+                          </Link>
+                        );
+                      })}
+                    </ul>
+                  </section>
+                </section>
+              </motion.div>
             )}
           </AnimatePresence>
         </section>
