a4f356c47095e3d036598842a83f5db90e433c46
StR0vErBG <xcreaw1@abv.bg>
2022-12-13 16:58:03 +0200

    fixing title & meta tags

--- pages/_app.js
+++ pages/_app.js
@@ -5,7 +5,16 @@ import Head from "next/head";
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Head></Head>
+      <Head>
+        <title>Movex - Транспортни и хамалски услуги</title>
+        <meta
+          name="description"
+          content="Movex Bulgaria предлага професионлани услуги в сферата на транспорта и
+          хамалските услуги. Като това включва преместване на дома, преместване
+          на офиса, международно преместване, транспортни услуги,
+          товаро-разтоварни дейности и преместване на пиана."
+        />
+      </Head>
       <Layout>
         <Component {...pageProps} />
       </Layout>
