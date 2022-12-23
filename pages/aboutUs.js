fe71e4f89c7a5772eee8f92ad627d08dd923bffa
StR0vErBG <xcreaw1@abv.bg>
2022-12-23 18:48:47 +0200

    fixing about page

--- pages/aboutUs.js
+++ pages/aboutUs.js
@@ -19,23 +19,32 @@ export default function AboutUs() {
           <div className="absolute top-0 z-10 w-screen h-screen bg-dark-transparent"></div>
         </div>
       </div>
-      <section className="relative z-20 pt-16 bg-white smToXl:container xl:mx-80 ">
+      <section className="relative z-20 pt-20 bg-white smToXl:container xl:mx-80 ">
         <div className="flex-col flex-center">
-          <Title title="Movex Bulgaria" size="text-5xl" />
-          <p className="max-w-lg mt-5 text-center">
-            Фирмата Movex Bulgaria е специализира в сферата на хамалогията и
-            логистиката, с дълги години усърдна работа сме постигнали голяма
-            аудитория от клиенти и смятаме да продължаваме да го поддържаме и
-            развиваме
+          <Title title="Нашата мисия" size="text-4xl" />
+
+          <p className="max-w-xl mt-5 text-center">
+            Да станем предпочитаната компания за всеки нуждаещ се от преместване
+            в страната и чужбина – прилагайки иновативни практики, качествени
+            услуги и решения. Да осигурим ефективно преместване на нашите
+            клиенти и най-доброто изживяване от прехода към новото си място. С
+            помощта на нашите всеотдайни професионалисти да създадем устойчивост
+            за растеж на бизнеса и обществото. Позволете да помогнем на вас за
+            един плавен преход без стрес.
           </p>
         </div>
         <section className="grid mt-20 lg:grid-cols-2 ">
           <div className="p-10 border-orange-100 border-y flex-center max-md:order-2">
             <div className="flex-col flex-center">
-              <Title title="Как работим?" size="text-3xl" />
+              <Title title="Визия" size="text-3xl" />
               <p className="mt-2 text-sm text-center md:max-w-sm ">
-                Бързо, лесно и прецизно с цел професионално изпълнение на
-                услугите ни.
+                В „Мувекс България“ имаме ясна визия за това, къде искаме да
+                стигнем и как да стигнем там. Вярваме, че като си поставяме
+                амбициозни цели и работим усилено за постигането им, можем да се
+                подобряваме и растем като компания. Нашата визия ни кара да
+                предоставяме най-добрите хамалски и транспортни услуги на
+                клиентите и да бъдем водещият избор за всеки, който се нуждае от
+                качествено и надеждно преместване.
               </p>
             </div>
           </div>
@@ -50,18 +59,50 @@ export default function AboutUs() {
           </div>
           <div className="p-10 border-b border-orange-100 flex-center max-md:order-4">
             <div className="flex-col flex-center">
-              <Title title="Нашите ценности" size="text-3xl" />
-              <ul className="max-w-md mt-2 text-sm list-disc">
-                <li>На първо място сте Вие</li>
-                <li>Уважение</li>
-                <li>Организираност</li>
+              <Title title="Ценности:" size="text-3xl" />
+              <ol className="flex flex-col max-w-md gap-2 mt-2 text-sm list-decimal">
+                <li>
+                  <h3 className="text-lg">Отговорност:</h3>
+                  <p>
+                    Поемаме отговорност за нашите действия и тяхното
+                    въздействие, държим да изградим доверие и надеждност за
+                    нашите клиенти и обществото. Да бъдем ангажирани, отговорни
+                    и почтени към вас е наш приоритет и ценност.
+                  </p>
+                </li>
+                <li>
+                  <h3 className="text-lg">Почтеност: / Честност:</h3>
+                  <p>
+                    Нашата основа на доверието и надеждността. Високо ценим
+                    честността и прозрачността във всички бизнес сделки, така
+                    ние създаваме на дълготрайни взаимоотношения с нашите
+                    заинтересовани страни и общността.
+                  </p>
+                </li>
+                <li>
+                  <h3 className="text-lg">Сътрудничество:</h3>
+                  <p>
+                    Нашият ключ към успеха в „Мувекс България“. Вярваме, че
+                    работата в екип и споделянето на идеи води към по-добри
+                    постижения. Изграждането на положителна и подкрепяща работна
+                    среда, която насърчава нашите служители и стимулира бизнес
+                    успеха. Работата в екип прави мечтата да работи.
+                  </p>
+                </li>
 
-                <li>Постоянна ангажираност и съпричасност към клиента</li>
-                <li>Баланс между качеството и ефективност</li>
-              </ul>
+                <li>
+                  <h3 className="text-lg">Иновативност:</h3>
+                  <p>
+                    Насърчаване на нашата креативност и непрекъснато
+                    усъвършенстване е начинът и движещата сила към основата на
+                    съвършенството в предоставянето на възможно най-добрите
+                    услуги за нашите клиенти и общността.
+                  </p>
+                </li>
+              </ol>
             </div>
           </div>
-          <div className="py-5  flex-center bg-orange md:py-10 max-md:order-3">
+          <div className="py-5 flex-center bg-orange md:py-10 max-md:order-3">
             <div className="relative w-20 h-20 md:h-44 md:w-44 ">
               <Image
                 src="/about-us/respect.png"
@@ -74,11 +115,18 @@ export default function AboutUs() {
             <div className="flex-col flex-center">
               <Title title="Нашата история" size="text-3xl" />
               <p className="max-w-md mt-2 text-sm text-center">
-                Бяхме млади и амбициозни студенти с голямо желание за развитие,
-                както финансово така и физически, затова открихме най-добрият
-                вариант за нас и това е хамалогията. През 2019 година нашето
-                хоби се превърна в професионален бизнес с името “Movex Bulgaria”
-                и до ден днешен не е спирал да се равива.
+                2017 година - основателя започва работа като доставчик, възмутен
+                от отношението на шефа си, се мотивира да създаде собствен малък
+                бизнес.
+              </p>
+              <p className="max-w-md mt-2 text-sm text-center">
+                Средата на 2017 година - започва работа в международна фирма за
+                товари
+              </p>
+              <p className="max-w-md mt-2 text-sm text-center">
+                2018 година - основателя Денислав започва съвместен бизнес с
+                Валери като вътрешена дъщерна транспортна фирма на Маднес
+                Експрес.
               </p>
             </div>
           </div>
@@ -92,6 +140,7 @@ export default function AboutUs() {
             </div>
           </div>
         </section>
+
         <section className="flex-col py-16 flex-center ">
           <div className="flex-center">
             <Title title="ЕКИПЪТ" size="text-4xl" />
