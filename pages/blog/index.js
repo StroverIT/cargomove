b1e0b069588dc1998f35af839d40919f786bb0a2
StR0vErBG <xcreaw1@abv.bg>
2023-01-05 23:42:47 +0200

    items center blog

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -23,7 +23,7 @@ const Index = () => {
               size="text-3xl"
             />
           </div>
-          <section className="grid mt-5 lg:grid-cols-2">
+          <section className="grid mt-5 lg:grid-cols-2 place-items-center">
             {blogData.map((data) => {
               return (
                 <Box
