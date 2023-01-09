6dd2ac649823b8d5664edc090ce16fe5a204ffe5
StR0vErBG <xcreaw1@abv.bg>
2023-01-09 15:56:34 +0200

    adding small icon

--- pages/_app.js
+++ pages/_app.js
@@ -7,9 +7,11 @@ function MyApp({ Component, pageProps }) {
     <>
       <Head>
         <title>Хамалски и транспортни услуги за гр. София, страната и ЕС</title>
+        <link rel="icon" href="/LOGODODO-01.svg" />
+
         <meta
           name="description"
-          content="Strover Company изработва, поддържа и развива сайтове. Предлага услуги в сферата на дигиталният маркетинг, като включва SEO, създаване, поддържане и развиване на социални мрежи, брандиране и ребрандиране. За всичко което Ви трябва да е на едно място, на достъпна цена с прекрасно обслужване."
+          content="Movex Bulgaria | Услуги в сферата на пренасянето и транспорта в гр. София, страната и ЕС"
         />
         <meta
           property="og:title"
@@ -24,7 +26,7 @@ function MyApp({ Component, pageProps }) {
           name="thumbnail"
           content="https://www.linkpicture.com/q/Неозаглавен-1440-t-1440-пиксела.png"
         />
-        <meta property="og:url" content="https://strover.bg/" />
+        <meta property="og:url" content="https://movex.bg/" />
         <meta name="robots" content="index, follow" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="preload" />
