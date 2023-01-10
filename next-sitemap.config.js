266a917f2e17dfc401341f9150e6235efdaa964f
StR0vErBG <xcreaw1@abv.bg>
2023-01-10 08:37:02 +0200

    changing config for SEO

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,5 +2,10 @@
 module.exports = {
   siteUrl: "https://movex.bg",
   generateRobotsTxt: true, // (optional)
+  exclude: [
+    "/legal/terms-and-conditions",
+    "/legal/privacy-policy",
+    // "/legal/cookie-policy",
+  ],
   // ...other options
 };
