4e73209289adb9fa7c5da4a790376a48a15ce5fe
StR0vErBG <xcreaw1@abv.bg>
2023-01-11 14:53:56 +0200

    adding contact us

---
+++ components/indexPage/ContactWithBox.js
@@ -0,0 +1,177 @@
+import React, { useEffect, useRef, useState } from "react";
+
+import { FiPhoneCall } from "react-icons/fi";
+import { HiOutlineMail } from "react-icons/hi";
+import { FaViber } from "react-icons/fa";
+import InputIcons from "../form/Icons";
+
+import Input from "../form/Input";
+import { motion, useAnimation } from "framer-motion";
+import { useInView } from "react-intersection-observer";
+
+import { toastSuccess, toastError } from "../../libs/Notifications";
+import sendMesage from "../../fetches/sendingMessage";
+import Script from "next/script";
+
+import ReCAPTCHA from "react-google-recaptcha";
+const inputsInit = {
+  name: "",
+  email: "",
+  message: "",
+};
+export default function ContactWithBox({
+  color,
+  bgColor,
+  btnColor,
+  number,
+  scrollBarThumb,
+  scrollBarTrack,
+  typePage,
+  mx = "3xl:mx-96",
+}) {
+  const [inputs, setInputs] = useState(inputsInit);
+
+  const [isLoading, setLoading] = useState(false);
+  const [box, boxView] = useInView();
+  const boxAnim = useAnimation();
+
+  const handler = (e) => {
+    setInputs((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
+  const recaptchaRef = useRef(null);
+  // border-pink
+  // bg-pink-50
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    recaptchaRef.current.execute();
+  };
+  const onReCAPTCHAChange = async (captchaCode) => {
+    // If the reCAPTCHA code is null or undefined indicating that
+    // the reCAPTCHA was expired then return early
+    if (!captchaCode) {
+      return;
+    }
+    try {
+      const response = await fetch("/api/register", {
+        method: "POST",
+        body: JSON.stringify({ captcha: captchaCode }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      if (response.ok) {
+        // If the response is ok than show the success alert
+        console.log("Email registered successfully");
+        setLoading(true);
+        // Execute the reCAPTCHA when the form is submitted
+
+        if (!inputs.name || !inputs.email || !inputs.message) {
+          setLoading(false);
+          return toastError("Моля попълнете всички полета!");
+        }
+
+        const data = await sendMesage(inputs);
+        if (data.message) {
+          setInputs(inputsInit);
+          toastSuccess(data.message);
+        }
+        if (data.error) {
+          toastError(data.error);
+        }
+
+        setLoading(false);
+      } else {
+        // Else throw an error with the message returned
+        // from the API
+        const error = await response.json();
+        throw new Error(error.message);
+      }
+    } catch (error) {
+      console.log(error?.message || "Something went wrong");
+    } finally {
+      // Reset the reCAPTCHA when the request has failed or succeeeded
+      // so that it can be executed again if user submits another email.
+      recaptchaRef.current.reset();
+    }
+  };
+  useEffect(() => {
+    if (boxView) boxAnim.start("animate");
+    if (!boxView) boxAnim.start("initial");
+  }, [boxView]);
+
+  return (
+    <>
+      <Script
+        strategy="afterInteractive"
+        id="my-script"
+        src={`https://www.google.com/recaptcha/api.js?render=${process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY}`}
+      ></Script>
+      <motion.section
+        ref={box}
+        animate={boxAnim}
+        className={`flex-center  max-3xl:container  `}
+        id="contactUs"
+      >
+        <section className={`  w-full px-5   `}>
+          <section className="">
+            <form
+              action=""
+              className="flex flex-col mt-6 "
+              onSubmit={submitHandler}
+            >
+              <ReCAPTCHA
+                ref={recaptchaRef}
+                size="invisible"
+                sitekey={process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY}
+                onChange={onReCAPTCHAChange}
+              />
+              <input
+                type="text"
+                value={inputs.name}
+                onChange={handler}
+                name="name"
+                placeholder="Вашето име"
+                className="py-2 pl-2 text-lg border border-gray-600 text-dark placeholder:text-gray-250"
+              />
+              <input
+                type="text"
+                value={inputs.email}
+                onChange={handler}
+                name="email"
+                placeholder="Вашият и-мейл"
+                className="py-2 pl-2 mt-4 text-lg border border-gray-600 text-dark placeholder:text-gray-250"
+              />
+              <div className="relative mt-5 lg:mb-5">
+                {/* className="w-full h-2 px-4 pt-2 bg-transparent border-b border-white scrollbar scrollbar-thumb-pink-100 scrollbar-track-pink-200 focus:outline-none" */}
+                <textarea
+                  className={`w-full  border-gray-600 py-2 pl-2 relative z-10   placeholder-transparent bg-transparent border appearance-none placeholder:pl-10 peer text-gray-darker focus:outline-none focus:shadow-outline scrollbar `}
+                  id="message"
+                  placeholder="Вашият коментар"
+                  name="message"
+                  value={inputs.message}
+                  rows="5"
+                  onChange={handler}
+                />
+                <label
+                  className="absolute block mb-2 text-sm transition-all duration-300 -top-4 -left-0 text-gray-250 peer-placeholder-shown:text-lg peer-placeholder-shown:px-6 peer-placeholder-shown:top-2 peer-placeholder-shown:-left-4"
+                  htmlFor="comment"
+                >
+                  Вашето съобщение
+                </label>
+              </div>
+              <button
+                className={`${btnColor} py-2  flex-center text-white`}
+                type="submit"
+              >
+                {isLoading ? <div className="loader"></div> : "Изпрати"}
+              </button>
+            </form>
+          </section>
+        </section>
+      </motion.section>
+    </>
+  );
+}
---
+++ pages/contactUs.js
@@ -0,0 +1,67 @@
+import Head from "next/head";
+import React, { useState } from "react";
+import { FaViber } from "react-icons/fa";
+import { FiPhoneCall } from "react-icons/fi";
+import { HiOutlineMail } from "react-icons/hi";
+import ContactWithBox from "../components/indexPage/ContactWithBox";
+
+const ContactUs = () => {
+  const [inputs, setInputs] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+  return (
+    <>
+      <Head>
+        <title>Movex - Контакти - Свържи се с нас </title>
+        <meta
+          name="description"
+          content="Movex - Контакти с нас. Винаги готови да отговорим на вашите въпроси. Може да се свържете с нас чрез телефон, и-мейл, viber, messenger и instagram "
+        />
+      </Head>
+      <main className="container mt-40 rounded-md shadow-lg">
+        <div className="grid grid-cols-[27%75%] container py-10">
+          <div className="pl-5 border-r border-gray-600">
+            <h1 className="pb-4 text-2xl font-semibold">Контакти</h1>
+            <ul className="pr-4">
+              <li className="flex items-center py-4 gap-x-2">
+                <div className="text-2xl">
+                  <FiPhoneCall />
+                </div>
+                <div className="pl-2">
+                  <h2 className="font-medium">Телефон</h2>
+                  <p>+359 89 506 3670</p>
+                </div>
+              </li>
+              <li className="flex items-center py-4 border-gray-600 gap-x-2 border-y">
+                <div className="text-2xl">
+                  <FaViber />
+                </div>
+                <div className="pl-2">
+                  <h2 className="font-medium">Viber</h2>
+                  <p>+359 89 506 3670</p>
+                </div>
+              </li>
+              <li className="flex items-center py-4 gap-x-2">
+                <div className="text-3xl">
+                  <HiOutlineMail />
+                </div>
+                <div className="pl-2">
+                  <h2 className="font-medium">И-мейл</h2>
+                  <p>movexbulgaria@gmail.com</p>
+                </div>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <h1 className="pb-4 pl-5 text-2xl font-semibold">Пишете ни</h1>
+            <ContactWithBox btnColor="bg-blue" />
+          </div>
+        </div>
+      </main>
+    </>
+  );
+};
+
+export default ContactUs;
