b2ab3a81931d79cb998dcc39e1419b176d8e4da7
emil <emil@mclimate.eu>
2023-10-17 11:44:04 +0300

    adding delete

--- pages/gallery.js
+++ pages/gallery.js
@@ -3,8 +3,35 @@ import React from "react";
 import { connectMongo } from "../db/connectDb";
 import GalleryModel from "../db/models/Gallery";
 import Image from "next/image";
+import { getSession } from "next-auth/react";
+import User from "../db/models/User";
+import { HiX } from "react-icons/hi";
+import { useRouter } from "next/router";
 
-export default function Gallery({ gallery }) {
+export default function Gallery({ gallery, user }) {
+  const router = useRouter();
+
+  const refreshData = () => {
+    router.replace(router.asPath);
+  };
+
+  const deleteImageHandler = async (_id) => {
+    const res = await fetch("/api/gallery/delete", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        imageId: _id,
+      }),
+    });
+    const data = await res.json();
+
+    if (data.message) {
+      refreshData();
+    }
+  };
+
   return (
     <section className="container mt-40">
       <h3 className="text-3xl">Нашата галерия</h3>
@@ -16,7 +43,23 @@ export default function Gallery({ gallery }) {
               key={image._id}
               className="relative flex flex-col items-center justify-center h-[400px] w-[400px]"
             >
-              <Image src={image.imageUrl} alt={image.alt} fill={true} className="object-contain" />
+              <Image
+                src={image.imageUrl}
+                alt={image.alt}
+                fill={true}
+                className="object-contain"
+              />
+              {user.role == "admin" && (
+                <div className="absolute top-0 right-0">
+                  <div
+                    className="absolute top-0 right-0 text-4xl text-secondary "
+                    id="close"
+                    onClick={() => deleteImageHandler(image._id)}
+                  >
+                    <HiX />
+                  </div>
+                </div>
+              )}
             </article>
           );
         })}
@@ -28,9 +71,14 @@ export default function Gallery({ gallery }) {
 export async function getServerSideProps(context) {
   await connectMongo();
 
+  const session = await getSession({ req: context.req });
   const gallery = await GalleryModel.find({});
+  const user = await User.findOne({ email: session.user.email });
 
   return {
-    props: { gallery: JSON.parse(JSON.stringify(gallery)) },
+    props: {
+      gallery: JSON.parse(JSON.stringify(gallery)),
+      user: JSON.parse(JSON.stringify(user)),
+    },
   };
 }
