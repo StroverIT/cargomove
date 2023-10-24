1997c64ad7a605e3480424878f0b7da79851aa9a
emil <emil@mclimate.eu>
2023-10-24 08:22:57 +0300

    jsx conventaion

--- components/form/ImageUpload.js
+++ components/form/ImageUpload.js
@@ -5,12 +5,12 @@ export default function ImageUpload({holder="Upload File", onChange, name="image
     <>
       <label
         class="block mb-2 text-sm font-medium text-[#111827] dark:text-white"
-        for="file_input"
+        htmlFor="file_input"
       >
         {holder}
       </label>
       <input
-        class="block w-full text-sm text-[#111827] border border-[#d1d5db] rounded-lg cursor-pointer bg-[#f9fafb] dark:text-[#9ca3af] focus:outline-none dark:bg-[#374151] dark:border-[#4b5563] dark:placeholder-[#9ca3af]"
+        className="block w-full text-sm text-[#111827] border border-[#d1d5db] rounded-lg cursor-pointer bg-[#f9fafb] dark:text-[#9ca3af] focus:outline-none dark:bg-[#374151] dark:border-[#4b5563] dark:placeholder-[#9ca3af]"
         id="file_input"
         name={name}
         type="file"
