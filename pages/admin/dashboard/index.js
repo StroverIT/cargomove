b159bdfa96b21a110ba232872796fc5968c44df0
StroverIT <xcreaw1@abv.bg>
2024-03-25 15:55:48 +0200

    switching to container

--- pages/admin/dashboard/index.js
+++ pages/admin/dashboard/index.js
@@ -7,6 +7,8 @@ import PageContainer from "../../../components/pages/admin/dashboard/PageContain
 // Mongodb
 import User from "../../../db/models/User";
 import { connectMongo } from "../../../db/connectDb";
+import { FaTools } from "react-icons/fa";
+import Container from "../../../components/pages/admin/dashboard/Container";
 
 const imageDataList = {
   gallery: {
@@ -23,20 +25,23 @@ const imageDataList = {
 };
 export default function Index() {
   return (
-    <section className="mt-40">
+    <section className="">
       <section className="container grid gap-6 my-5 lg:grid-cols-3 lg:mt-10 gap-y-10">
+        <Container icon="home" text={"Начало"} link={"/"} />
+
         <PageContainer
           icon="undefined"
           text="Галери"
           link="gallery/main"
           imageData={imageDataList.gallery}
-        />{" "}
+        />
         <PageContainer
           icon="undefined"
           text="Блог"
           link="blog/create"
           imageData={imageDataList.blog}
         />
+        <PageContainer icon="tools" text="Услуги" link="services/create" />
       </section>
     </section>
   );
@@ -44,13 +49,13 @@ export default function Index() {
 
 export async function getServerSideProps(context) {
   const { query } = context;
-  
+
   const session = await getSession({ req: context.req });
 
   await connectMongo();
-  
+
   const user = await User.findOne({ email: session?.user?.email });
-  
+
   if (user?.role != "admin" || !user || !session) {
     return {
       redirect: {
